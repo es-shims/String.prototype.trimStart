3341104450bc6ac84f3b70a6d6c0fbeb4df5131e
Jordan Harband <ljharb@gmail.com>
2020-03-29 23:11:39 -0700

    [Tests] add `npm run lint`

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-String.prototype.trimStart = String.prototype.trimStart ? String.prototype.trimStart : function() {
-	if(String.prototype.trimLeft) {
+'use strict';
+
+// eslint-disable-next-line no-extend-native, consistent-return
+String.prototype.trimStart = String.prototype.trimStart || function () {
+	if (String.prototype.trimLeft) {
 		return this.trimLeft();
-	} else if(String.prototype.trim) {
+	}
+	if (String.prototype.trim) {
 		var trimmed = this.trim();
 		var indexOfWord = this.indexOf(trimmed);
-		
+
 		return this.slice(indexOfWord, this.length);
 	}
 };
